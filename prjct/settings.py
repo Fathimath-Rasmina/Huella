cc37edb1e2b66d8035018ad936e90d10573e7de9
Fathimath-Rasmina <rasminaf58@gmail.com>
2022-10-29 12:00:51 +0530

    database

--- prjct/settings.py
+++ prjct/settings.py
@@ -86,8 +86,13 @@ AUTH_USER_MODEL = 'accounts.Accounts'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'huella',
+        'USER': 'huella',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
